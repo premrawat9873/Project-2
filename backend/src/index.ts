2fe99b1769697fda8ca5704ad8ce9b1ad2fb5b10
PremRawat <premrawat9873@gmail.com>
2026-02-15 01:01:37 +0530

    Adding Route and updated DBURL

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,19 +1,34 @@
 import { Hono } from 'hono'
-
+import { PrismaClient } from './generated/prisma/client'
+import { withAccelerate } from '@prisma/extension-accelerate'
 const app = new Hono()
 
-app.post('/api/v1/signup', (c) => {
-  return c.text('Hello Hono!')
+app.post('/api/v1/signup', async (c) => {
+    const prisma = new PrismaClient({
+      // @ts-ignore
+    accelerateUrl: c.env.ACC_DATABASE_URL,
+  }).$extends(withAccelerate())
+  const body = await c.req.json();
+  await prisma.user.create({
+    data: {
+      email: body.email,
+      password: body.password
+    }
+  })
 })
+
 app.post('/api/v1/signin', (c) => {
   return c.text('Hello Hono!')
 })
+
 app.post('/api/v1/blog', (c) => {
   return c.text('Hello Hono!')
 })
+
 app.put('/api/v1/blog', (c) => {
   return c.text('Hello Hono!')
 })
+
 app.get('/api/v1/blog/:id', (c) => {
   return c.text('Hello Hono!')
 })
